7c343fd7313fe0e6835c587c53b6115a0e3d826e
root <root@recette-manager.manager.cartouche.com>
2014-03-26 16:33:56 +0100

    ddd

--- src/Manager/HABundle/Controller/DefaultController.php
+++ src/Manager/HABundle/Controller/DefaultController.php
@@ -8,33 +8,35 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
 	public function indexAction(){
-		$mha='/etc/mha.conf';
+		$mha = '/etc/mha.conf';
 
-		$user=`cat $mha | grep user | awk '{print $3}'`;
-		$password=`cat $mha | grep password | awk '{print $3}'`;
-		$ip_bdd=explode('\n', (`cat $mha | grep hostname | awk '{print $3}'`));
-		 
-		echo $user; 
-		echo $password;
-		var_dump($ip_bdd);
-		
-		 
-		$array1 = array('ip' => '172.20.0.225',
+		$user = `cat $mha | grep user | awk '{print $3}'`;
+		$password = `cat $mha | grep password | awk '{print $3}'`;
+		$ip_bdd = explode("\n", `cat $mha | grep hostname | awk '{print $3}'`);
+		$ip_bdd = array_filter($ip_bdd); 
+		$data =array();
+	
+		foreach($ip_bdd as $ip){ 
+			$array1 = array('ip' => '172.20.0.225',
 				'status' => 'Master',
 				'log_binaire' => 'Maria-00097.bin',
 				'pos_binaire' => '548752',
 				'io_running' => ' ',
 				'sql_running' => ' ');
-		 
+			
+			array_push($data, $array1);
+		} 
+		/*
 		$array2 = array('ip' => '172.20.0.236',
 				'status' => 'Slave',
 				'log_binaire' => 'Maria-00097.bin',
 				'pos_binaire' => '548752',
 				'io_running' => 'Yes',
 				'sql_running' => 'Yes');
-		$array = array($array1, $array2);
+		*/
+		//$array = array($array1, $array2);
 		 
-		return $this->render('ManagerHABundle:Default:index.html.twig', array('bdd' => $array));
+		return $this->render('ManagerHABundle:Default:index.html.twig', array('bdd' => $data));
 	}
 
 	public function logAction(){
@@ -42,6 +44,18 @@ class DefaultController extends Controller
 		$response = new Response(json_encode($html));
 		$response->headers->set('Content-Type', 'application/json');
 		return $response;
+	}
+
+public function isMaster() {
+		return `mysql -h $1 -u $user -p$password -N -s -e "select variable_value from information_schema.global_status where variable_name = 'Slave_running';"`;
+	}
+
+	public function getMasterStatus() {
+		return `mysql -h $1 -u $user -p$password -N -s -e "SHOW MASTER STATUS;"`;
+	}
+
+	public function getSlaveStatus() {
+		return `mysql -h $1 -u $user -p$password -e "SHOW SLAVE STATUS\G;"`;
 	}
 
 }
