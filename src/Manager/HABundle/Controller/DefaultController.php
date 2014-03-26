b2368493b480158b7beb2ab85b766d65e8ac0359
johnny <johnny.cottereau@auguria.net>
2014-03-26 16:42:19 +0100

    c

--- src/Manager/HABundle/Controller/DefaultController.php
+++ src/Manager/HABundle/Controller/DefaultController.php
@@ -13,29 +13,45 @@ class DefaultController extends Controller
 		$user = `cat $mha | grep user | awk '{print $3}'`;
 		$password = `cat $mha | grep password | awk '{print $3}'`;
 		$ip_bdd = explode("\n", `cat $mha | grep hostname | awk '{print $3}'`);
-		$ip_bdd = array_filter($ip_bdd); 
+		$ip_bdd = array_filter($ip_bdd);
 		$data =array();
-	
-		foreach($ip_bdd as $ip){ 
-			$array1 = array('ip' => '172.20.0.225',
-				'status' => 'Master',
-				'log_binaire' => 'Maria-00097.bin',
-				'pos_binaire' => '548752',
-				'io_running' => ' ',
-				'sql_running' => ' ');
+
+		foreach($ip_bdd as $ip){
+			$array = $status = null;
+			$isMaster = isMaster($ip);
+				
+			if($isMaster == 'OFF'){
+				$status = getMasterStatus($ip);
+				var_dump($status);
+				$array = array('ip' => $ip,
+						'status' => 'Master',
+						'log_binaire' => 'Maria-00097.bin',
+						'pos_binaire' => '548752',
+						'io_running' => ' ',
+						'sql_running' => ' ');
+				
+			} elseif($isMaster == 'ON' ){
+				$status = getSlaveStatus ($ip);
+				
+				var_dump($status);
+				$array = array('ip' => $ip,
+						'status' => 'Master',
+						'log_binaire' => 'Maria-00097.bin',
+						'pos_binaire' => '548752',
+						'io_running' => ' ',
+						'sql_running' => ' ');
+				
+				$log_binaire = `echo $status | awk -F":" '{ print $8 }' | awk -F" " '{ print $1 }'`;
+				$pos_binaire = `echo $status | awk -F":" '{ print $7 }' | awk -F" " '{ print $1 }'`;
+				$io_running = `echo $status | awk -F":" '{ print $12 }' | awk -F" " '{ print $1 }'`;
+				$sql_running = `echo $status | awk -F":" '{ print $13 }' | awk -F" " '{ print $1 }'`;
+			}
+
+			if(! is_null($array)){
+				array_push($data, $array);
+			}
+		}
 			
-			array_push($data, $array1);
-		} 
-		/*
-		$array2 = array('ip' => '172.20.0.236',
-				'status' => 'Slave',
-				'log_binaire' => 'Maria-00097.bin',
-				'pos_binaire' => '548752',
-				'io_running' => 'Yes',
-				'sql_running' => 'Yes');
-		*/
-		//$array = array($array1, $array2);
-		 
 		return $this->render('ManagerHABundle:Default:index.html.twig', array('bdd' => $data));
 	}
 
@@ -46,16 +62,18 @@ class DefaultController extends Controller
 		return $response;
 	}
 
-public function isMaster() {
-		return `mysql -h $1 -u $user -p$password -N -s -e "select variable_value from information_schema.global_status where variable_name = 'Slave_running';"`;
+
+
+	public function isMaster($user, $password, $ip) {
+		return `mysql -h $ip -u $user -p$password -N -s -e "select variable_value from information_schema.global_status where variable_name = 'Slave_running';"`;
 	}
 
-	public function getMasterStatus() {
-		return `mysql -h $1 -u $user -p$password -N -s -e "SHOW MASTER STATUS;"`;
+	public function getMasterStatus($user, $password, $ip) {
+		return `mysql -h $ip -u $user -p$password -N -s -e "SHOW MASTER STATUS;"`;
 	}
 
-	public function getSlaveStatus() {
-		return `mysql -h $1 -u $user -p$password -e "SHOW SLAVE STATUS\G;"`;
+	public function getSlaveStatus($user, $password, $ip) {
+		return `mysql -h $ip -u $user -p$password -e "SHOW SLAVE STATUS\G;"`;
 	}
 
 }
