a1a276cb847a5e7a2e57d6635904a7cea116d006
johnny <johnny.cottereau@auguria.net>
2014-03-26 16:16:25 +0100

    e

--- src/Manager/HABundle/Controller/DefaultController.php
+++ src/Manager/HABundle/Controller/DefaultController.php
@@ -7,31 +7,39 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-    	$array1 = array('ip' => '172.20.0.225',
-    					'status' => 'Master',
-    					'log_binaire' => 'Maria-00097.bin',
-    					'pos_binaire' => '548752',
-    					'io_running' => ' ',
-    					'sql_running' => ' ');
-    	
-    	$array2 = array('ip' => '172.20.0.236',
-		    			'status' => 'Slave',
-		    			'log_binaire' => 'Maria-00097.bin',
-		    			'pos_binaire' => '548752',
-    					'io_running' => 'Yes',
-    					'sql_running' => 'Yes');
-    	$array = array($array1, $array2);
-    	
-        return $this->render('ManagerHABundle:Default:index.html.twig', array('bdd' => $array));
-    }
-    
-    public function logAction(){
+	public function indexAction(){
+		$mha='/etc/mha.conf';
+
+		$user=`cat $mha | grep user | awk '{print $3}'`;
+		$password=`cat $mha | grep password | awk '{print $3}'`;
+		$ip_bdd=(`cat $mha | grep hostname | awk '{print $3}'`);
+		 
+		echo $user; 
+		
+		 
+		$array1 = array('ip' => '172.20.0.225',
+				'status' => 'Master',
+				'log_binaire' => 'Maria-00097.bin',
+				'pos_binaire' => '548752',
+				'io_running' => ' ',
+				'sql_running' => ' ');
+		 
+		$array2 = array('ip' => '172.20.0.236',
+				'status' => 'Slave',
+				'log_binaire' => 'Maria-00097.bin',
+				'pos_binaire' => '548752',
+				'io_running' => 'Yes',
+				'sql_running' => 'Yes');
+		$array = array($array1, $array2);
+		 
+		return $this->render('ManagerHABundle:Default:index.html.twig', array('bdd' => $array));
+	}
+
+	public function logAction(){
 		$html = nl2br(shell_exec('tail -n 15 /var/log/masterha/MHA.log'));
-    	$response = new Response(json_encode($html));
-    	$response->headers->set('Content-Type', 'application/json');
-    	return $response;
-    }
-    
+		$response = new Response(json_encode($html));
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
 }
