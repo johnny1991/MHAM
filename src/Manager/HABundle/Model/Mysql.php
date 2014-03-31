89384a4eb8d3c1fcb8c4083cb71417d6ec5dfd3f
johnny <johnny.cottereau@auguria.net>
2014-03-31 18:59:57 +0200

    sdf

--- src/Manager/HABundle/Model/Mysql.php
+++ src/Manager/HABundle/Model/Mysql.php
@@ -57,7 +57,7 @@ class Mysql {
 		if(!$this->state){
 			try {
 				$result = $this->PDOinstance->query("select variable_value from information_schema.global_status where variable_name = 'Slave_running'")->fetch();
-				$this->state = ($result['variable_value'] == 'ON') ? 'Master' : 'Slave';
+				$this->state = ($result['variable_value'] == 'OFF') ? 'Master' : 'Slave';
 			} catch (PDOException $e) {
 				return $e;
 			}
@@ -72,7 +72,6 @@ class Mysql {
 					$this->replicationStatus = $this->PDOinstance->query("SHOW MASTER STATUS")->fetch();
 				} else if($this->state == 'Slave'){
 					$this->replicationStatus = $this->PDOinstance->query("SHOW SLAVE STATUS")->fetch();
-					var_dump($this->PDOinstance->query("SHOW SLAVE STATUS")->fetch());
 				}
 			} catch(Exception $e) {
 				return false;
