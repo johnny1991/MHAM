ad4d802a7f52d829893caa04c5442cc4d23d764b
johnny <johnny.cottereau@auguria.net>
2014-04-01 14:47:34 +0200

    sdfg

--- src/Manager/HABundle/Model/MagentoServer.php
+++ src/Manager/HABundle/Model/MagentoServer.php
@@ -24,7 +24,7 @@ class MagentoServer extends Server {
 	public function initLocalxml(){
 		$string = shell_exec("/home/installer_mha/getLocalXml --user=root --ip=$this->ip --path=$this->localxmlpath");
 		$this->localxml = simplexml_load_string($string);
-		$this->BddIp = $this->localxml['global']['resources']['default_setup']['connection']['host'];
+		$this->BddIp = $this->localxml->global->resources->default_setup->connection->host;
 		print_r($this->localxml);
 	}
 
