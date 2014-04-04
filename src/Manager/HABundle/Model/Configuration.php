ca4e9c982583aafd8fa8752d7296a9d3c03e261f
johnny <johnny.cottereau@auguria.net>
2014-04-04 15:14:38 +0200

    sdf

--- src/Manager/HABundle/Model/Configuration.php
+++ src/Manager/HABundle/Model/Configuration.php
@@ -5,13 +5,12 @@ namespace Manager\HABundle\Model;
 class Configuration {
 
 	public $conf;
-	
+
 	public function __construct(){
-		echo __DIR__.'../../../../scripts/HA.conf';
-		$this->conf = parse_ini_file(__DIR__.'../../../../scripts/HA.conf', 1, INI_SCANNER_RAW);
+		$this->conf = parse_ini_file(__DIR__.'../../../../../scripts/HA.conf', 1, INI_SCANNER_RAW);
 	}
-	
+
 	public function getConf(){
-		return $this>conf;
+		return $this->conf;
 	}
 }
