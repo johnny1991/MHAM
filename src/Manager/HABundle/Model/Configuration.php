94b0d1e000e10f302f8b4a950bf768cb1ceaa74b
johnny <johnny.cottereau@auguria.net>
2014-04-07 10:02:57 +0200

    qsd

--- src/Manager/HABundle/Model/Configuration.php
+++ src/Manager/HABundle/Model/Configuration.php
@@ -28,7 +28,7 @@ class Configuration {
 	public $local_xml_path;
 
 	protected function __construct(){
-		$this->scripts_path = __DIR__ . '../../../../../scripts/';
+		$this->scripts_path = __DIR__ . '/../../../../../scripts/';
 		$this->configuration_path = $this->scripts_path . 'HA.conf';
 		$this->configuration = parse_ini_file($this->configuration_path, 1, INI_SCANNER_RAW);
 
