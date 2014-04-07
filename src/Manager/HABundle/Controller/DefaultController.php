f094edf1364f4d5419b676a1f2aaa6b0b5025b86
johnny <johnny.cottereau@auguria.net>
2014-04-07 10:50:08 +0200

    kk

--- src/Manager/HABundle/Controller/DefaultController.php
+++ src/Manager/HABundle/Controller/DefaultController.php
@@ -19,9 +19,9 @@ class DefaultController extends Controller
 	}
 
 	public function syncAction(){
-		$response = shell_exec("/bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getMainBddIp());
+		$response = exec("/bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getMainBddIp());
 		
-		echo "/usr/bin/sudo /bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getMainBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp();
+		echo "/bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getMainBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp();
 		
 		return new response($response);
 	}
