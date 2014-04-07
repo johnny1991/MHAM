1c3f6340fc94878b9059966b47fefafbba176eb3
johnny <johnny.cottereau@auguria.net>
2014-04-07 10:43:40 +0200

    ff

--- src/Manager/HABundle/Controller/DefaultController.php
+++ src/Manager/HABundle/Controller/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends Controller
 	public function syncAction(){
 		$response = shell_exec("/usr/bin/sudo /bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getMainBddIp());
 		
-		echo "/usr/bin/sudo /bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getMainBddIp();
+		echo "/usr/bin/sudo /bin/bash " . Configuration::getInstance()->getScriptsPath(). "synchronize --ip_master_to_slave=" . ManagerMHA::getInstance()->getMha()->getMainBddIp() . " --ip_slave_to_master=" . ManagerMHA::getInstance()->getMha()->getSlaveBddIp();
 		
 		return new response($response);
 	}
