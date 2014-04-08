a6fe337bd36cd2cd4cf5216443888e186e72de51
johnny <johnny.cottereau@auguria.net>
2014-04-08 14:28:48 +0200

    sdfgh,

--- src/Manager/HABundle/Model/Mysql.php
+++ src/Manager/HABundle/Model/Mysql.php
@@ -107,7 +107,6 @@ class Mysql {
 				foreach ($tmp as $item){
 					$result[$item['Variable_name']] = $item['Value'];
 				}
-				var_dump($result);
 				$this->global = $result;
 			} catch(Exception $e) {
 				return false;
